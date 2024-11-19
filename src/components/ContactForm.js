bfd2271ed720a43aa6e7422e9bf9b5a07c8ffb38
your-username <your-email@example.com>
2024-11-19 20:01:15 +0600

    Frontend update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import './ContactFormStyles.css';
+import React, { useState, useEffect } from "react";
+import "./ContactFormStyles.css";
 
 const ContactPage = () => {
+  const [contactInfo, setContactInfo] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/contact/") 
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Ошибка при загрузке данных");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (Array.isArray(data) && data.length > 0) {
+          setContactInfo(data[0]); 
+        }
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (!contactInfo) {
+    return <div className="contact-container">Загрузка данных...</div>;
+  }
+
   return (
     <div className="contact-container">
-      <p><strong>Address:</strong> XXXXXXXXXXXXXXX</p>
-      <p><strong>Phone:</strong> +996 777 111 222</p>
-      <p><strong>Phone:</strong> +996 555 222 333</p>
-      <p><strong>Email:</strong> kyrgyz_venture@tour.kg</p>
-        <p><strong>Facebook</strong> <a href="/">
-            <i className="fa-brands fa-facebook-square"></i>
-        </a></p>
-        <p><strong>Instagram</strong><a href="/">
-            <i className="fa-brands fa-instagram-square"></i>
-        </a></p>
-          
+      <p><strong>Address:</strong> {contactInfo.address}</p>
+      <p><strong>Phone:</strong> {contactInfo.phone1}</p>
+      <p><strong>Phone:</strong> {contactInfo.phone2}</p>
+      <p><strong>Email:</strong> <a href={`mailto:${contactInfo.email}`}>{contactInfo.email}</a></p>
+      <p>
+        <strong>Facebook:</strong>
+        <a href={contactInfo.facebook} target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-facebook-square"></i>
+        </a>
+      </p>
+      <p>
+        <strong>Instagram:</strong>
+        <a href={contactInfo.instagram} target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-instagram-square"></i>
+        </a>
+      </p>
     </div>
   );
 };
